f7eb08abe3da9cc5d116911c26bb80883bbda816
Kyle G. Capobianco-Hogan <kgcapohogan@gmail.com>
2021-12-27 15:46:37 -0500

    Remove use of provides_args argument for Signal
    Fixes #23

--- channels_presence/signals.py
+++ channels_presence/signals.py
@@ -1,5 +1,6 @@
 from django import dispatch
 
-presence_changed = dispatch.Signal(
-    providing_args=["room", "added", "removed", "bulk_change"]
-)
+# Provides the arguments `"room"`, `"added"`, `"removed"`, and
+# `"bulk_change"` to listeners (specified by the `providing_args`
+# argument to `Signal` prior to Django 4.0).
+presence_changed = dispatch.Signal()
